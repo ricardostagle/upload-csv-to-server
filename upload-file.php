fce76426fe172b780ec70e45ee0b5957a3052c72
ricardostagle <ricardo.stagle@gmail.com>
2018-04-04 17:38:21 -0500

    removing white space

--- upload-file.php
+++ upload-file.php
@@ -16,32 +16,23 @@
 </body>
 </html>
 <?PHP
-  if(!empty($_FILES['uploaded_file']))
-  {
+  if(!empty($_FILES['uploaded_file'])){
     $path = "uploads/";
     $path = $path . basename( $_FILES['uploaded_file']['name']);
     if(!empty(basename( $_FILES['uploaded_file']['name'])) && basename( $_FILES['uploaded_file']['name']) == "uploads/"){
       unlink($path);
     }
-    
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-      
       $filename=basename( $_FILES['uploaded_file']['name']);
       echo "<br>El archivo '". $filename ."' ha sido cargado.<br>".PHP_EOL;
-
       if(substr($filename, -3) == 'csv'){
-        
         echo "<br>Comienza a ejecutar php...".PHP_EOL;
-
         //run_request($filename); 
-
       }else{
         echo "<br>La extencion del archivo debe de ser csv.<br>".PHP_EOL;
       }
-     
     } else{
       echo "<br>Hubo un error cargando el archivo, por favor vuelva a intentar.<br>".PHP_EOL;
     }
   }
-
 ?>
